dc9b4fa3cff955cb7599f546ee5f21bb0b90692b
canadyj4900 <canadyj4900@student.faytechcc.edu>
2024-06-16 22:08:22 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_canadyjosiah.py
@@ -0,0 +1,7 @@
+# josaiah canady
+# 6/16/2024
+# P1LAB1 Instructions
+# programm will take first and last name and then itroducew you to the class
+name =input("Enter your first name: ")
+namen =input("Enter your last  name: ")
+print("hello, " +name+ " " +namen+ "! Welcome to CTI-110")
