a33163e3f16974a57f0d35fbbaf32a4de98eb72d
canadyj4900 <canadyj4900@student.faytechcc.edu>
2024-06-17 20:21:37 -0400

    Add files via upload
    meme

---
+++ P1HW1_canadyjosiah.py
@@ -0,0 +1,21 @@
+# josaiah canady
+# 6/16/2024
+# P1LAB1 Instructions
+print("-----Calculating Exponets-----")
+print("")
+print("")
+x =int(input("Enter an integer as the base value: ")) 
+y =int(input("Enter an integer as the base exponent: ")) 
+ans= (x ** y)
+print(x, "raised to the power of", y, "is" , ans, "!!")
+print("")
+print("")
+print("-----Adition and Subtraction-----")
+print("")
+print("")
+u =int(input("Enter a starting integer: "))
+i =int(input("Enter an integer to add: "))
+o =int(input("Enter an integer to subtract: "))
+an= ( u+i-o)
+print("")
+print(u," + ",i, " - " ,o , "is equal to" ,an) 
