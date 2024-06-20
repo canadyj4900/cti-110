4fa50a87ee96de92352b96251b52a9607d19a172
canadyj4900 <canadyj4900@student.faytechcc.edu>
2024-06-20 17:49:53 -0400

    Add files via upload
    cheese

---
+++ P2LAB1_canadyjosiah.py
@@ -0,0 +1,18 @@
+# josaiah canady
+# 6/16/2024
+# P1HW2 Instructions
+# code to perform the circle formulas quick and easy 
+z= float(input('What is the radius of the circle?:'))
+x= (z*2)
+y= round( x, 1)
+print('')
+print('The diameter of the circle is',y)
+a=(3.141592653589793*z*2)
+b= round(a,2)
+print('')
+print('The circumfrence of the circle is',b)
+q=(z*z)
+p=(q*3.141592653589793)
+o=round(p,3)
+print('')
+print('The area of the circle is', o)
