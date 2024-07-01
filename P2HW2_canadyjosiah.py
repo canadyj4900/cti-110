a579415bf44228d207f7a65803b862d26c3c9ded
canadyj4900 <canadyj4900@student.faytechcc.edu>
2024-07-01 16:38:55 -0400

    ss

---
+++ P2HW2_canadyjosiah.py
@@ -0,0 +1,21 @@
+a=float(input('Enter grade for Module 1:'))
+b=float(input('Enter grade for Module 2:'))
+c=float(input('Enter grade for Module 3:'))
+d=float(input('Enter grade for Module 4:'))
+e=float(input('Enter grade for Module 5:'))
+f=float(input('Enter grade for Module 6:'))
+print('')
+print('------------Results------------')
+grade_list=[a,b,c,d,e,f]
+list_min=min(grade_list)
+print(f"Lowest Grade: {list_min}")
+list_max=max(grade_list)
+print(f"Highest Grade: {list_max}")
+list_sum=sum(grade_list)
+print(f"Sum of Grades: {list_sum}")
+x=(a+b+c+d+e+f)
+y=(x/6)
+print("Sum of Grades:",end="")
+print(f'{y:.2f}')
+
+
