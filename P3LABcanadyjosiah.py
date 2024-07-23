8b1687dae925f22b7a19b2005d1157363fec129f
canadyj4900 <canadyj4900@student.faytechcc.edu>
2024-07-23 16:38:35 -0400

    P3LABcanadyjosiah.py

--- P3LABcanadyjosiah.py
+++ P3LABcanadyjosiah.py
@@ -2,48 +2,59 @@ change= float(input('Enter the amount of monry as a float:'))
 if change == 0:
     print("No change needed")
 
-num_dollars = change // 100
-change = change- (num_dollars * 100)
+num_dollars = change / 100
+x = change - (num_dollars * 100)
+a= num_dollars * 100
 
-num_quaters = change // 25
-change = change - (num_quaters * 25)
 
-num_dimes = change // 10
-change = change - (num_dimes * 25)
+num_quaters = change / 25
+y = change - (num_quaters * 25)
+s= num_quaters
 
-num_nickles = change // 5
-change = change - (num_nickles * 5)
+num_dimes = change / 10
+z = change - (num_dimes * 25)
 
-num_pennies= change // 5
-change = change - (num_pennies * 1)
+num_nickles = change / 5
+c = change - (num_nickles * 5)
 
-if num_dollars>0:
-    print(num_dollars, end="")
-    if num_dollars == 1:
-        print("dollar")
+num_pennies= change / 5
+v = change - (num_pennies * 1)
+
+if a > 0:
+    print(f'{a:.0f}', end="")
+    if a == 1:
+        print(" dollar")
     else:
-        print("dollars")
-if num_quaters>0:
-    print(num_quaters, end="")
-    if num_dollars == 1:
-        print("quater")
+        print(" dollars")
+
+
+if s > 0:
+    print(f'{s:.0f}', end="")
+    if s == 1:
+        print(" quater")
     else:
-        print("quaters")
-if num_dimes>0:
+        print(" quaters")
+
+
+if num_dimes > 0:
     print(num_dimes, end="")
     if num_dimes == 1:
-        print("dime")
+        print(" dime")
     else:
-        print("dimes")
-if num_nickles>0:
+        print(" dimes")
+
+
+if num_nickles > 0:
     print(num_nickles, end="")
     if num_nickles == 1:
-        print("nickle")
+        print(" nickle")
     else:
-        print("nickles")
+        print(" nickles")
+ 
+
 if num_pennies > 0:
     print(num_pennies, end="")
     if num_pennies  == 1:
-        print("pennies")
+        print(" pennies")
     else:
-        print("pennies")
+        print(" pennies")
